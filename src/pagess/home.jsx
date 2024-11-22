451c175aec7186d5a092cbf7ebc147fd55d7738f
João Vitor Parizotto Benedetti <108908521+Nopri010@users.noreply.github.com>
2024-11-22 23:45:05 +0000

    ultimas alterações

--- src/pagess/home.jsx
+++ src/pagess/home.jsx
@@ -1,66 +1,76 @@
-import React, { useState, useEffect, useContext } from "react";
 import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+import { useContext } from "react";
+import { useState, useEffect } from "react";
+import Header from "../components/Header";
 import { UserContext } from "../context/UserContext";
-import { Header } from "../components/Header";
+
 export function Home() {
-  const { user } = useContext(UserContext);
   const [map, setMap] = useState(null);
   const [mapCenter, setMapCenter] = useState({
     lat: -28.2624,
     lng: -52.396032,
   });
+
+  const [markers, setMarkers] = useState([]);
+
+  const { user } = useContext(UserContext);
+
   const containerStyle = {
     width: "100%",
-    height: "100%",
+    height: "80%",
+  };
+
+  const center = {
+    lat: -3.745,
+    lng: -38.523,
+  };
+
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "",
+  });
+
+  const onLoad = (map) => {
+    const bounds = new window.google.maps.LatLngBounds(mapCenter);
+    map.fitBounds(bounds);
+
+    setMap(map);
   };
 
   const handleMapClick = (event) => {
     const marker = {
-      latitude: event.LatLng.Lat(),
-      longitude: event.LatLng.Lng(),
+      latitude: event.latLng.lat(),
+      longitude: event.latLng.lng(),
     };
-    const { markers, serMarkers } = useStates([]);
 
     fetch("https://apibase2-0bttgosp.b4a.run/ws/foco", {
       method: "POST",
       headers: {
-        "Contenti-Type": "application/json",
-        Authorization: "Bearer ${User} ",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${user}`,
       },
       body: JSON.stringify(marker),
     })
       .then((response) => response.json())
       .then(() => {
-        console.log("Novo ponto cadastrado");
+        console.log("Novo ponto cadastrado com sucesso!");
         setMarkers([
           ...markers,
-          {
-            lat: marker.latitude,
-            lng: marker.longitude,
-          },
+          { lat: marker.latitude, lng: marker.longitude },
         ]);
       })
       .catch((error) => {
-        alert("Erro ao cadastrar");
+        alert("Erro ao cadastrar novo ponto");
       });
   };
 
-  const { isLoaded } = useJsApiLoader({
-    id: "google-map-script",
-    googleMapsApiKey: "############",
-  });
-
-  const onLoad = (map) => {
-    const bounds = new window.google.maps.LatLngBounds(mapCenter);
-    map.fitBounds(map);
-
-    setMap(map);
-  };
-
+  // Pega a localização atual do usuário
   useEffect(() => {
+    console.log("Carregou o componente");
     const location = window.navigator && window.navigator.geolocation;
 
-    const locationSuccess = (position) => {
+    const locationSucess = (position) => {
+      console.log(position);
       setMapCenter({
         lat: position.coords.latitude,
         lng: position.coords.longitude,
@@ -75,57 +85,54 @@ export function Home() {
     };
 
     if (location) {
-      location.getCurrentPosition(locationSuccess, locationError);
+      console.log("Entrou no if");
+      location.getCurrentPosition(locationSucess, locationError);
     }
   }, []);
 
+  //carregar todos os marcadores do usuario da API
   useEffect(() => {
     fetch("https://apibase2-0bttgosp.b4a.run/ws/foco", {
       headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer ${user}",
+        "Content-Type": "Application/json",
+        Authorization: `Bearer ${user}`,
       },
-    }).then((response) => response.json());
-    then((data) => {
-      const newMarkersArray = data.map(({ latitude, longitude }) => {
-        return {
-          lat: latitude,
-          lng: longitude,
-        };
-      });
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const newMarkerArray = data.map(({ latitude, longitude }) => {
+          return {
+            lat: latitude,
+            lng: longitude,
+          };
+        });
 
-      setMarkers(newMarkersArray);
-    }).catch((error) => {
-      console.log(error);
-      alert("Erro ao buscar pontos cadastrados");
-    });
+        setMarkers(newMarkerArray);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Erro ao buscar pontos cadastrados");
+      });
   }, []);
 
   return (
     <>
-      <Header>
-        {isLoaded && (
-          <GoogleMap
-            mapContainerStyle={containerStyle}
-            center={mapCenter}
-            zoom={8}
-            onLoad={onLoad}
-            onClick={handleMapClick}
-            onUnmount={() => setMap(null)}
-          >
-            {markers.map((marker, key) => (
-              <Merker key={key} position={marker} />
-            ))}
-            <></>
-          </GoogleMap>
-        )}{" "}
-        : (<></>
-        );
-      </Header>
+      <Header />
+
+      {isLoaded && (
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={mapCenter}
+          zoom={10}
+          onLoad={onLoad}
+          onClick={handleMapClick}
+          onUnmount={() => setMap(null)}
+        >
+          {markers.map((marker, key) => (
+            <Marker key={key} position={marker} />
+          ))}
+        </GoogleMap>
+      )}
     </>
   );
 }
-
-/**
- * Quando o usuário fizer login, seus dados serão "salvos" aqui
- */
